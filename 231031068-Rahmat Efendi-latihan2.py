2d5b378f33feafef188b57a3fa89c90a0610d86f
rahmatefe <153784733+rahmatefe@users.noreply.github.com>
2023-12-21 15:44:31 +0800

    upload latihan2

---
+++ 231031068-Rahmat Efendi-latihan2.py
@@ -0,0 +1,15 @@
+#latihan 2
+
+inp = input('Masukan input:')
+
+if len(inp) != 3:
+    print('Panjang string', len(inp), 'tidak sama dengan string 3')
+else:
+    print('Panjang input sesuai yang diinginkan')
+
+inp = input('Masukkan suatu input: ')
+if len(inp) != 3:
+ print('Panjang string ', len(inp),'tidak sama dengan 3')
+else:
+ print('Panjang input sesuai yang diinginkan')
+ 
